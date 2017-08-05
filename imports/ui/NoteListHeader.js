e8ca9a1e58fdacc4826e7c41e6830a523fb135b2
Tolu Kalejaiye <mrkalejaiye@gmail.com>
2017-08-05 18:43:20 +0100

    added focus for newly created note and sort by update time

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { Session} from 'meteor/session';
 
 export const NoteListHeader = (props) => {
   return (
     <div>
       <button onClick={() => {
-        props.meteorCall('notes.insert');
+        props.meteorCall('notes.insert', (err, res) => {
+          if(res) {
+            props.Session.set('selectedNoteId', res);
+          }
+        });
       }}> Add Note </button>
     </div>
   );
@@ -15,10 +20,12 @@ export const NoteListHeader = (props) => {
 
 NoteListHeader.PropTypes = {
   meteorCall: PropTypes.func.isRequired,
+  Session: PropTypes.object.isRequired
 }
 
 export default createContainer(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session
   };
 }, NoteListHeader);
